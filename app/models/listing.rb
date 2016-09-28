b22fa36039244c14247d516da27a59c7d79e6113
Topher St. John <christopher.stjohn1994@gmail.com>
2016-09-28 12:10:33 -0500

    images dont work

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,4 @@
 class Listing < ActiveRecord::Base
+  has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "NotFound.png"
+  validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
 end
